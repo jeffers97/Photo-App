ecaf11d11869b98ed85587c4c9d27046ca312592
S. Tunji Turner <Sweetie@Audra-jefferss-MacBook-Pro.local>
2014-03-29 13:47:29 -0400

    Initial Commit

---
+++ Photo App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photo App
+//
+//  Created by S. Tunji Turner on 3/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
